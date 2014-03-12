0917d4b6b801179a7bd58e8cfec145ad5f9f0759
Terry Worona <tworona@jawbone.com>
2014-03-12 16:47:31 -0700

    more opacity work

--- JBChartViewDemo/JBChartViewDemo/Constants/JBColorConstants.h
+++ JBChartViewDemo/JBChartViewDemo/Constants/JBColorConstants.h
@@ -28,6 +28,6 @@
 #define kJBColorLineChartHeader UIColorFromHex(0x1c474e)
 #define kJBColorLineChartHeaderSeparatorColor UIColorFromHex(0x8eb6b7)
 #define kJBColorLineChartDefaultSolidLineColor [UIColor colorWithWhite:1.0 alpha:0.5]
-#define kJBColorLineChartDefaultSolidSelectedLineColor [UIColor colorWithWhite:0.0 alpha:1.0]
+#define kJBColorLineChartDefaultSolidSelectedLineColor [UIColor colorWithWhite:1.0 alpha:1.0]
 #define kJBColorLineChartDefaultDashedLineColor [UIColor colorWithRed:1.0 green:0.0 blue:0.0 alpha:1.0]
 #define kJBColorLineChartDefaultDashedSelectedLineColor [UIColor colorWithWhite:1.0 alpha:1.0]
