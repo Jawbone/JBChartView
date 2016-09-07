524cf10f706412dc06a2ad3c68245b7d3fdbd654
Sasha Heinen <sheinen@pivotal.io>
2016-09-07 10:20:23 -0600

    Added shared framework for Carthage consumption

---
+++ JBChartView/JBChartView/JBChartView.h
@@ -0,0 +1,30 @@
+//
+//  JBChartView.h
+//  JBChartView
+//
+//  Created by pivotal on 9/7/16.
+//  Copyright © 2016 pivotal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JBChartView.
+FOUNDATION_EXPORT double JBChartViewVersionNumber;
+
+//! Project version string for JBChartView.
+FOUNDATION_EXPORT const unsigned char JBChartViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JBChartView/PublicHeader.h>
+
+#import <JBChartView/NSMutableArray+JBStack.h>
+#import <JBChartView/JBBarChartView.h>
+#import <JBChartView/JBGradientBarView.h>
+#import <JBChartView/JBChartView.h>
+#import <JBChartView/JBLineChartView.h>
+#import <JBChartView/JBGradientLineLayer.h>
+#import <JBChartView/JBShapeLineLayer.h>
+#import <JBChartView/JBLineChartLine.h>
+#import <JBChartView/JBLineChartPoint.h>
+#import <JBChartView/JBLineChartDotsView.h>
+#import <JBChartView/JBLineChartDotView.h>
+#import <JBChartView/JBLineChartLinesView.h>
