221025252a2392a361142d6b429824c0d7c3fd89
Cody Sehl <csehl@pivotal.io>
2016-09-07 13:49:22 -0600

    Fixed inclusion issues with Carthage
    Signed-off-by: Sasha Heinen <sheinen@pivotal.io>
    Signed-off-by: Cody Sehl <csehl@pivotal.io>

---
+++ JBChartView/JBChartView/JBChart.h
@@ -0,0 +1,30 @@
+//
+//  JBChartView.h
+//  JBChartView
+//
+//  Created by pivotal on 9/7/16.
+//  Copyright © 2016 pivotal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JBChartView.
+FOUNDATION_EXPORT double JBChartViewVersionNumber;
+
+//! Project version string for JBChartView.
+FOUNDATION_EXPORT const unsigned char JBChartViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JBChart/PublicHeader.h>
+
+#import <JBChart/NSMutableArray+JBStack.h>
+#import <JBChart/JBBarChartView.h>
+#import <JBChart/JBGradientBarView.h>
+#import <JBChart/JBChartView.h>
+#import <JBChart/JBLineChartView.h>
+#import <JBChart/JBGradientLineLayer.h>
+#import <JBChart/JBShapeLineLayer.h>
+#import <JBChart/JBLineChartLine.h>
+#import <JBChart/JBLineChartPoint.h>
+#import <JBChart/JBLineChartDotsView.h>
+#import <JBChart/JBLineChartDotView.h>
+#import <JBChart/JBLineChartLinesView.h>
